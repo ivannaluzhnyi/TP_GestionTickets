d1d88e7b0a64b8cc8b53bb469fd9f462152b7764
Ivan Naluzhnyi <Ivan Naluzhnyi@PC-NALUZHNYI>
2017-10-05 14:13:59 +0200

    probleme d'affichage

--- application/controllers/Ctrl_GestionTickets.php
+++ application/controllers/Ctrl_GestionTickets.php
@@ -21,7 +21,7 @@ class Ctrl_GestionTickets extends CI_Controller{
           $this->load->library('form_validation');
                  if($this->form_validation->run()==TRUE)
                  {    
-                     $this->load->helper('url');
+                    /*  $this->load->helper('url');
                       $this->load->helper('form');
                       $this->load->view('v_accueil');
                       
@@ -33,8 +33,53 @@ class Ctrl_GestionTickets extends CI_Controller{
 		 }
                 else{
                     $this->load->model("Model_connexion");
-                    $data['lesLogPass'] = $this->Model_connexion->getLoginPass(); 
+                    /*$data['lesLogins'] = $this->Model_connexion->getLogin();
+                    $data['lesPass'] = $this->Model_connexion->getPass();
+                    print_r($data);*/
                     
+                            $dataIn = array( 
+                            'loginUserV' => $this->input->post('loginUser'),
+                            'pwdUserV' => $this->input->post('pwdUser') 
+                            ); 
+                            print_r($dataIn); 
+                    
+                    $lesLogPass =$data['lesLogPass'] = $this->Model_connexion->getLogPass();
+                   // print_r($lesLogPass);
+                    $tab1=array();
+                    $tab=array();
+                 // $donne = array_merge($tab1,$data['lesLogPass']);
+                 // $donne1 = array_merge($tab,$donne['stdClass Object']);
+                  
+                 // print_r($donne1);
+                //  print_r($donne);
+                  
+                    foreach ($lesLogPass as $lp){
+                        $lp->loginUser;
+                        $lp->pwdUser;
+                        $donne = array_merge($tab,$lp);
+                        print_r($lp);
+                    }
+                    print_r($donne);
+                   
+                       /* if($lesLogPass->loginUser==$dataIn['loginUserV']&&$lesLogPass->pwdUser==$dataIn['pwdUserV']){
+                             $this->load->view('v_lesTickets', $this->afficherLesTickets());
+                             echo "JooooooooooooooooooooooPPPPPP";
+                        }
+                        else{
+                            echo "JJJJJJJJJJJSSSSSSSSSPPPPPPPPPPP";
+                            $this->load->helper('url');
+                            //$this->load->helper('form');
+                            $this->load->view("v_accueil");
+                        }*/
+                }
+                        
+                     
+                    
+                   
+                    
+                    
+                    
+                   /* 
                     $tab=array();
                     $tab1=array();
                     $donne = array_merge($tab,$data['lesLogPass']);
@@ -46,22 +91,14 @@ class Ctrl_GestionTickets extends CI_Controller{
                                 print_r($key);
                         }*/
                             
-                        }
+                        
                         //$donne1 = array_merge($tab1,$d['stdClass Object']);
                        // print_r($donne1);
                     
                     //print_r($donne);
-                    
-                    $data = array( 
-                    'loginUser' => $this->input->post('loginUser'),
-                    'pwdUser' => $this->input->post('pwdUser') 
-                    ); 
-                    
-                    print_r($data);                    
-               
 
-                    $this->load->view('v_lesTickets', $this->afficherLesTickets());//$data);
-                }
+                 //   $this->load->view('v_lesTickets', $this->afficherLesTickets());//$data);
+                
 }
 
         public function afficherLesTickets(){
--- application/models/Model_connexion.php
+++ application/models/Model_connexion.php
@@ -2,8 +2,26 @@
 
 class Model_connexion extends CI_Model{
 
-    public function getLoginPass(){
-        $sql = $this->db->query("select loginUser,pwdUser from users");
+    public function getLogin(){
+        $sql = $this->db->query("select loginUser from users");
+           $tab=array();
+         foreach ($sql->result()as $row){
+             $tab[]=$row;
+         }
+       return $tab;
+    }
+    
+    public function getPass(){
+        $sql = $this->db->query("select pwdUser from users");
+           $tab=array();
+         foreach ($sql->result()as $row){
+             $tab[]=$row;
+         }
+       return $tab;
+    }
+    
+    public function getLogPass(){
+        $sql = $this->db->query("select loginUser, pwdUser from users");
            $tab=array();
          foreach ($sql->result()as $row){
              $tab[]=$row;
